8fa4d99108f55f684445e86d7850fd5d3b96d554
EricDosReis <ericdosreis.personal@gmail.com>
2025-03-25 16:28:15 -0300

    refactor: move components to landing-page folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import { FAQ } from '@/components/FAQ';
-import { Header } from '@/components/Header';
-import { Hero } from '@/components/Hero';
-import { Pricing } from '@/components/Pricing';
-import { VideoExplanation } from '@/components/VideoExplanation';
+import { FAQ } from "@/components/landing-page/FAQ";
+import { Header } from "@/components/landing-page/Header";
+import { Hero } from "@/components/landing-page/Hero";
+import { Pricing } from "@/components/landing-page/Pricing";
+import { VideoExplanation } from "@/components/landing-page/VideoExplanation";
 
 export default function Home() {
   return (
