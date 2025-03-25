b8c857724b693b1f1a1a05de0882d0aacbb74025
EricDosReis <ericdosreis.personal@gmail.com>
2025-03-25 14:48:51 -0300

    feat: create Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,20 @@
+import { Button } from "@/components/ui/Button";
+import Image from "next/image";
+
 const Header = () => {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="absolute top-0 left-0 right-0 max-w-7xl mx-auto flex items-center justify-between py-10">
+      <div className="flex items-center gap-4">
+        <Image src="/logo.svg" alt="MyProjects logo" width={27} height={32} />
+        <h1 className="text-white text-2xl font-bold">MyProjects</h1>
+      </div>
+
+      <div className="flex items-center gap-4">
+        <Button>My page</Button>
+        <Button variant="ghost">Logout</Button>
+      </div>
     </header>
-  )
-}
+  );
+};
 
 export { Header };
