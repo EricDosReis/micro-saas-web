4a9baa9ee7b83e50376d8de3c6b8ad6ded6ae653
EricDosReis <ericdosreis.personal@gmail.com>
2025-03-25 15:19:43 -0300

    feat: create VideoExplanation component

--- src/components/VideoExplanation/VideoExplanation.tsx
+++ src/components/VideoExplanation/VideoExplanation.tsx
@@ -1,9 +1,13 @@
+import { Play } from "lucide-react";
+
 const VideoExplanation = () => {
   return (
-    <section>
-      <h2>VideoExplanation</h2>
+    <section className="flex justify-center">
+      <div className="w-[800px] rounded-xl bg-gray-800 border border-white/10 aspect-video flex items-center justify-center my-20 hover:cursor-pointer">
+        <Play className="text-purple-500 size-16" />
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export { VideoExplanation };
